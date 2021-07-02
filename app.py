d9a3933c52f814a8ee2bbc59369f3444dcae5763
Josucka <frajolla27@gmail.com>
2021-07-02 11:58:30 -0300

    Acréscimo_de_conteudo
    Acrecentei algumas linhas de codigo ja com referencia nas devidas rotas

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/episodios')
+def Episodios():
+    return render_template('episodios.html')
+
+@app.route('/episodios/<play>') #o play tem que coloca com o id pra rota chamar a proxima pagina com o episodio que for selecionado
+def Play():
+    return render_template('play.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
