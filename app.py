18cae88d8b15ae66b0ae101fe6e7bb74d6830c6c
cleitonfurst <furstcleiton@gmail.com>
2021-07-04 16:33:36 -0300

    Alterações do player

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ def Opcao():
 
 @app.route('/episodios')
 def Episodios():
+    
+
     return render_template('episodios.html')
 
 @app.route('/play') #o play tem que coloca com o id pra rota chamar a proxima pagina com o episodio que for selecionado
 def Play():
+
+
     return render_template('play.html')
 
 if __name__ == '__main__':
